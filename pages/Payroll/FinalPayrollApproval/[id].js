1a30f50b4d5e4ab19b8b1af7eae86bbde23731d4
Gurukiran <sanath@amazeinc.in>
2023-04-24 18:58:35 +0530

    Payroll YTD component

---
+++ pages/Payroll/FinalPayrollApproval/[id].js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '@/components/Layout/index.js';
+
+const ID = () => {
+    return (
+        <Layout>
+
+        </Layout>
+    )
+}
+
+export default ID
