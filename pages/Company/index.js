baf94145688c3918195f058b5da02e31bed8f1dc
manjan <manjan@amazeinc.in>
2023-04-24 19:04:32 +0530

    changes

---
+++ pages/Company/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Companydashboard from '@/components/Dashboard/Company/index'
+const index=()=> {
+  return (
+    <div>
+     <Companydashboard></Companydashboard>
+    </div>
+  )
+}
+
+export default index
