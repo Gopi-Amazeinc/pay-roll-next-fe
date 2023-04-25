a30c0da61f953245f47a5d134482ccf9dd98491c
Gopi-amazeinc <gopi@amazeinc.in>
2023-04-25 15:48:50 +0530

    dashboard xl removed

--- pages/Home/dashboard.js
+++ pages/Home/dashboard.js
@@ -8,8 +8,6 @@ import { BiInjection } from "react-icons/bi";
 
 import Layout from "@/components/layout/layout.js";
 
-import * as XLSX from "xlsx";
-
 function Dashboard() {
   const count = 1;
 
@@ -19,37 +17,6 @@ function Dashboard() {
   const [viewMode, setViewMode] = useState("tab1");
 
   const [items, setItems] = useState([]);
-
-  const readExcel = async (file) => {
-    debugger;
-    const promise = new Promise((resolve, reject) => {
-      const fileReader = new FileReader();
-      fileReader.readAsArrayBuffer(file);
-
-      fileReader.onload = (e) => {
-        const bufferArray = e.target.result;
-
-        const wb = XLSX.read(bufferArray, { type: "buffer" });
-
-        const wsname = wb.SheetNames[0];
-
-        const ws = wb.Sheets[wsname];
-
-        const data = XLSX.utils.sheet_to_json(ws);
-
-        resolve(data);
-      };
-
-      fileReader.onerror = (error) => {
-        reject(error);
-      };
-    });
-    promise.then((d) => {
-      debugger;
-      setItems(d);
-      console.log(d);
-    });
-  };
 
   return (
     <Layout>
