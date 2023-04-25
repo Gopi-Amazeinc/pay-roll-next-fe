ed591f2218e63e05e4b26c1d4e5fdbd5d08fa59c
Gurunath <gurunath@amazeinc.in>
2023-04-25 15:26:11 +0530

    added ot components

--- components/Dashboard/Attendance/OvertimeUnitsUpload/index.js
+++ components/Dashboard/Attendance/OvertimeUnitsUpload/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
-import overtime from '../../styles/myteamovertimedetails.module.css'
+// import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
+// import overtime from '../../styles/myteamovertimedetails.module.css'
 // import Layout from '../../Components/layout.js';
 import { useRef } from 'react';
 import { DownloadTableExcel } from 'react-export-table-to-excel';
+import { Modal, ModalBody, ModalFooter } from 'reactstrap';
 
 const MyTeamOverTimeDetail = () => {
 
@@ -12,116 +13,112 @@ const MyTeamOverTimeDetail = () => {
   const [modalOpen, setModalOpen] = useState(false);
   return (
 
-      <div>
-        <div className="col-md-12">
-          <div className="row">
-          <h2>Yet to bind</h2>
-            <div className="col-md-12">
-              <ul>
-              </ul>
-            </div>
+    <div>
+      <div className="col-md-12">
+        <div className="row">
+          <h4>Yet to bind</h4>
+          <div className="col-md-12">
+            <ul>
+            </ul>
           </div>
-          <br></br>
-          <div className={overtime.card}>
-            <div className={overtime.filtercard}>
-              <div className="row">
-                <div className="col-md-1"> Filter By </div>
-                <div className="col-md-2">
-                  <p>Pay Date </p>
-                  <input type="date" onkeydown="return false" placeholder="Duration" id="sdate" name="sdate" className="form-control "></input>
-                </div>
-                <div className="col-md-4">
-                  <p></p><br></br>
-                  <input id="term" name="term" type="search" placeholder="Search for staff.. " className="form-control "></input>
-                </div>
-                <div className="col-md-3"><br></br><p></p>
-                  <Button className={overtime.button}
-                    color="primary"
+        </div>
+        <br></br>
+        <div className='card shadow-lg p-4 rounded-3 mt-1'>
+          {/* <div className={overtime.filtercard}> */}
+          <div className="row">
+            <div className="col-md-1"> Filter By </div>
+            <div className="col-md-2">
+              <p>Pay Date </p>
+              <input type="date" onkeydown="return false" placeholder="Duration" id="sdate" name="sdate" className="form-control "></input>
+            </div>
+            <div className="col-md-4">
+              <p></p><br></br>
+              <input id="term" name="term" type="search" placeholder="Search for staff.. " className="form-control "></input>
+            </div>
+            <div className="col-md-3"><br></br><p></p>
+              <button color="primary" className='submit-button' type="button" onClick={() => setModalOpen(!modalOpen)} >
+                Upload OVERTIME
+              </button>
+              <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen}>
+                <div className=" modal-header">
+                  <h5 className=" modal-title" >
+                    Upload Staff</h5>
+                  <button
+                    aria-label="Close"
+                    className=" close"
                     type="button"
                     onClick={() => setModalOpen(!modalOpen)}
                   >
-                    Upload OVERTIME
-                  </Button>
-                  <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen}>
-                    <div className=" modal-header">
-                      <h5 className=" modal-title" >
-                        Upload Staff</h5>
-                      <button
-                        aria-label="Close"
-                        className=" close"
-                        type="button"
-                        onClick={() => setModalOpen(!modalOpen)}
-                      >
-                        <span aria-hidden={true}>×</span>
-                      </button>
-                    </div>
-                    <ModalBody >
-                      <div className='row'>
-                        <div className='col-lg-6'>
-                          <input type="file" className={overtime.input} />
-                          <div className='row'>
-                            <ModalFooter>
-
-                              <Button className={overtime.UploadStaffButton} color="primary" type="button">
-                                Upload Staff
-                              </Button>
-                            </ModalFooter>
-                          </div>
-                        </div>
-                        <div className='col-lg-6'>
-                          <div>
-                            <DownloadTableExcel
-                              filename="users table"
-                              sheet="users"
-                              currentTableRef={tableRef.current}>
-                              <button type="button" className="form-control CancelBTN">Export To Excel </button>
-                            </DownloadTableExcel>
-                          </div>
-                        </div>
-                      </div>
-                    </ModalBody>
-                  </Modal>
+                    <span aria-hidden={true}>×</span>
+                  </button>
                 </div>
-              </div>
-            </div>
-          </div>
-          <br></br>
-          <div className="row">
-            <div className="col-lg-10">
-            </div>
-            <div className="col-lg-2"> <DownloadTableExcel
-              filename="UploadLoanTemplate"
-              sheet="users"
-              currentTableRef={tableRef.current}
-            >
-              <button className={overtime.button}>Export to Excel</button>
-            </DownloadTableExcel>
+                <ModalBody >
+                  <div className='row'>
+                    <div className='col-lg-6'>
+                      <input type="file" />
+                      <div className='row'>
+                        <ModalFooter>
 
+                          <button color="primary" type="button">
+                            Upload Staff
+                          </button>
+                        </ModalFooter>
+                      </div>
+                    </div>
+                    <div className='col-lg-6'>
+                      <div>
+                        <DownloadTableExcel
+                          filename="users table"
+                          sheet="users"
+                          currentTableRef={tableRef.current}>
+                          <button type="button" className="form-control CancelBTN">Export To Excel </button>
+                        </DownloadTableExcel>
+                      </div>
+                    </div>
+                  </div>
+                </ModalBody>
+              </Modal>
             </div>
           </div>
-          <br></br>
+          {/* </div> */}
+        </div>
+        <br></br>
+        <div className="row">
+          <div className="col-lg-10">
+          </div>
+          <div className="col-lg-2"> <DownloadTableExcel
+            filename="UploadLoanTemplate"
+            sheet="users"
+            currentTableRef={tableRef.current}
+          >
+            <button>Export to Excel</button>
+          </DownloadTableExcel>
+
+          </div>
+        </div>
+        <br></br>
+        <div className="row">
+          <div className="col-md-5">
+          </div>
+          <div className="col-md-5">
+          </div>
+          <div className="col-md-2">
+          </div>
+        </div>
+        <br></br>
+        <div className="row">
           <div className="row">
-            <div className="col-md-5">
-            </div>
-            <div className="col-md-5">
-            </div>
-            <div className="col-md-2">
-            </div>
           </div>
           <br></br>
-          <div className="row">
-            <div className="row">
+          <div>
+            <div><br></br><br></br>
             </div>
-            <br></br>
-            <div>
-              <div><br></br><br></br>
-              </div>
-            </div>
-            <div className="col-md-1">
-            </div>
+          </div>
+          <div className="col-md-1">
           </div>
         </div>
       </div>
+    </div>
   )
 }
 
---
+++ pages/OT/new.js
@@ -0,0 +1,158 @@
+import Layout from '@/Components/layout'
+import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+// import overtime from "../../styles/Requests/overtimedetailsform.module.css"
+import Modal from 'react-modal';
+import axios from 'axios';
+// import Styles from "../../styles/initialpayrolldetails.module.css";
+import Link from 'next/link';
+
+const Applyot = () => {
+   const [overtimeDetail, setOvertimeDetail] = useState([]);
+   useEffect(() => {
+      async function getData() {
+         let hostURL = process.env.NEXT_PUBLIC_API_HOST_URL;
+         // This API is used for fetch the Requests data in modal//
+         let res = await axios.get(hostURL + "HR/GetStaffOverTimeDetails");
+         setOvertimeDetail(res.data);
+      }
+      getData()
+   }, []);
+
+   const customStyles = {
+      content: {
+         top: '50%',
+         left: '50%',
+         right: 'auto',
+         marginRight: '-50%',
+         transform: 'translate(-50%, -50%)',
+         width: '60%'
+      }
+   };
+
+   const [modalIsOpen, setIsOpen] = useState(false);
+   const [date, getDate] = useState("")
+
+   function openModal() {
+      setIsOpen(true);
+   }
+
+   function closeModal() {
+      setIsOpen(false);
+   }
+   const insertdata = async (data) => {
+      debugger;
+      let hostURL = process.env.NEXT_PUBLIC_API_HOST_URL;
+      await axios.post(hostURL + "HR/InsertStaffOverTimeDetails", data);
+      console.log(data);
+   }
+
+
+   const { register, handleSubmit, reset, formState: { errors }, } = useForm();
+   return (
+      <Layout>
+         <div className="container-fluid">
+            <div className="row card shadow">
+               <form onClick={handleSubmit(insertdata)}>
+                  <div className="col-12">
+                     <p className="Heading">Add Actual Time</p>
+                  </div>
+                  <br />
+                  <br />
+                  <div className='row'>
+                     <div className='col-lg-3'>
+                        <p>Date Request <i className='text-danger'>*</i></p>
+                        <input type='date' className='form-control' {...register('Date_Request', { required: true })} />
+                        {errors.Date_Request && (<p className='text-danger mt-2'>Select Valid Date</p>)}
+                     </div>
+                     <div className='col-lg-3'>
+                        <p>Start Date <i className='text-danger'>*</i></p>
+                        <input type='time' className='form-control' {...register('Actuval_StartTime', { required: true })} />
+                        {errors.Actuval_StartTime && (<p className='text-danger mt-2'>Select Valid Start Time</p>)}
+                     </div>
+                     <div className='col-lg-3'>
+                        <p>End Date <i className='text-danger'>*</i></p>
+                        <input type='time' className='form-control' {...register('Actuval_EndTime', { required: true })} />
+                        {errors.Actuval_EndTime && (<p className='text-danger mt-2'>Select Valid End Time</p>)}
+                     </div>
+                  </div>
+                  <br />
+                  <br />
+                  <div className='row' >
+                     <div className='col-lg-3'>
+                        <p>Supporting Documents</p>
+                        <div className={overtime.drop} >
+                           <br />
+                           <input type="file" id="undefined" multiple="" accept=".png,.jpg,.jpeg,application/pdf" />
+
+                           <br />
+                           <br />
+                           Drop files here Only PDF,PNG,JPEG,JPG
+                           (max 5mb)
+
+
+                        </div>
+                     </div>
+                     <br />
+                     <br />
+                     <div className='col-lg-3'>
+                        <p>Purpose</p>
+                        <div ><textarea rows="3" id="Comments" name="Comments" type="text" placeholder="Write here..." className="form-control inputfield "></textarea></div>
+                     </div>
+                  </div >
+                  <div className="d-flex justify-content-end w-100 mt-2 mb-2 pr-2">
+                     <Link href="/OT/myovertimedetails"> <button className="submit-button" >Cancel</button></Link>
+                     <button className="submit-button">Submit</button><button data-toggle="modal" data-target="#mytestid" className="submit-button" onClick={openModal}>OT Details</button>
+                  </div>
+               </form>
+               <Modal isOpen={modalIsOpen} style={customStyles}>
+                  <div className='modalheader'>
+                     <div className='row'>
+                        <div className="col-lg-6">
+                           <h6 style={{ fontWeight: '500' }}>Component Details</h6>
+                        </div>
+                        <div className='col-lg-4'></div>
+                        <div className='col-lg-2'>
+                           <button className='btn btn-primary' onClick={closeModal}>Close</button>
+                        </div>
+                     </div>
+                     <hr></hr>
+                  </div>
+                  <div className='modalbody'>
+                     <div className="row">
+                        <div className='col-lg-12'>
+                           <table className='table table-bordered mt-4 text-center table-striped ' id={Styles.table}>
+                              <thead>
+                                 <tr id={Styles.tr}>
+                                    <th className='text-white'>OT Type</th>
+                                    <th className='text-white'>No of Hours </th>
+
+                                 </tr>
+                              </thead>
+                              <tbody >
+                                 {
+                                    overtimeDetail.map((data, index) => {
+                                       return (
+                                          <tr className="text-dark" key={index}>
+                                             <td>{data.componentName}</td>
+                                             <td>{data.componentValue}</td>
+                                             <td>{data.ded_type}</td>
+                                          </tr>
+                                       )
+                                    })
+                                 }
+                              </tbody>
+                           </table>
+                        </div>
+                     </div>
+                  </div>
+               </Modal>
+            </div>
+
+         </div>
+
+      </Layout >
+   )
+}
+
+export default Applyot;
