4c781accac0ec090f640047a261d7a560d2e3b2b
Gurunath <gurunath@amazeinc.in>
2023-04-25 11:05:44 +0530

    changes in attendence

---
+++ pages/Attendance/ShiftDetails/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Shiftdetails from '/components/Dashboard/Attendance/ShiftDetails/index'
+
+const ShiftDetails = () => {
+    return (
+        <div>
+            <Shiftdetails></Shiftdetails>
+        </div>
+    )
+}
+
+export default ShiftDetails
