51b34869e23947bd74935eaf45c4078a61a16166
Anant <anantpathak4@gmail.com>
2023-04-26 14:51:17 +0530

    settings

---
+++ pages/Settings/componentmapping/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '@/components/layout/layout.js'
+import ComponentMappingDashboard from '@/components/Dashboard/Settings/componentmapping'
+
+function index() {
+    return (
+        <Layout>
+            <ComponentMappingDashboard></ComponentMappingDashboard>
+        </Layout>
+    )
+}
+
+export default index
