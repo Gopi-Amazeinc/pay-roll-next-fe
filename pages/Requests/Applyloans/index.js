83dcf7f63965ce474f8d1bb339959e31ee5c417c
krishna <krishna@amazeinc.in>
2023-04-27 12:54:40 +0530

    Requests folders are restored

---
+++ pages/Requests/Applyloans/index.js
@@ -0,0 +1,12 @@
+import ApplyLoansDashboard from '@/components/Dashboard/Requests/Applyloans/index';
+import Layout from '@/components/layout/layout';
+const Index = () => {
+    return (
+        <div>
+            <Layout>
+            <ApplyLoansDashboard></ApplyLoansDashboard>
+            </Layout>
+        </div>
+    )
+}
+export default Index;
