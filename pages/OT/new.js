22c847d4e3de4a46d40d25946a86a5321d6dcfd3
krishna <krishna@amazeinc.in>
2023-05-03 18:53:48 +0530

    Get was done in OT

--- pages/OT/new.js
+++ pages/OT/new.js
@@ -1,73 +1,329 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Layout from "@/components/layout/layout";
+import Modal from 'react-modal';
+import { useForm } from 'react-hook-form';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import Styles from "@../../../pages/OT/Ot.module.css"
 const OverTimeDetails = () => {
+  const { register, handleSubmit, watch, reset, formState } = useForm();
+  const [dashboardData, setDashboardData] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const hostURL = process.env.NEXT_PUBLIC_API_HOST_URL;
+
+  const openEditModal = () => {
+    setModalOpen(true)
+  }
+  const closeModal = () => {
+    setModalOpen(false)
+  }
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      width: '60%',
+      height: '70%'
+    }
+  }
+
+  const getDetails = async () => {
+    var startTime = watch("StartTime");
+    var endTime = watch("EndTime");
+    var date = watch("Date");
+    var staffID = sessionStorage.getItem("userID");
+    const res = await axios.get(hostURL + "HR/GetOtNightOt?StartTime=" + startTime + "&EndTime=" + endTime + "&Shift=1&StaffID=" + staffID + "&Date=" + date);
+    setDashboardData(res.data);
+    console.log(res.data);
+    onSubmit(res.data);
+  }
+
+  async function onSubmit(data) {
+    try {
+      console.log("form data", data);
+
+    } catch (error) {
+
+    }
+  }
+  const insertDetails = async () => {
+    debugger;
+    try {
+      await axios.post(hostURL + "HR/InsertStaffOverTimeDetails", dashboardData);
+      Swal.fire('Data Inserted successfully')
+      console.log("Inserted data:", dashboardData);
+      location.href = ("/OT");
+    }
+    catch {
+      Swal.fire("Insert is not working");
+    }
+  }
+
   return (
     <Layout>
-      <div class="shadow-lg p-3 mb-5 bg-white rounded">
-        <div>
-          <h4 style={{ color: "blue" }}>Add Actual Time</h4>
-          <div className='row mt-4'>
-            <div className='col-lg-4'>
-              <p>Date Request<span style={{ color: "red" }}>*</span></p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="shadow-lg p-3 mb-5 bg-white rounded">
+          <div>
+            <h4 style={{ color: "blue" }}>Add Actual Time</h4>
+            <div className='row mt-4'>
+              <div className='col-lg-4'>
+                <p>Date Request<span style={{ color: "red" }}>*</span></p>
 
+              </div>
+              <div className='col-lg-2'>
+                <p>Actual Start Time<span style={{ color: "red" }}>*</span></p>
+              </div>
+              <div className='col-lg-2'>
+                <p>Actual End Time<span style={{ color: "red" }}>*</span></p>
+              </div>
             </div>
-            <div className='col-lg-4'>
-              <p>Actual Start Time<span style={{ color: "red" }}>*</span></p>
+            <div className='row'>
+              <div className='col-lg-4'>
+                <input type='date' className='form-control'  {...register('Date', { required: true })} ></input>
+              </div>
+              <div className='col-lg-2'>
+                {/* <TimePicker onChange={handleTimeChange} value={time} disableClock={true} clearIcon={null} /> */}
+                <input type='time' className='form-control' name='time' min="00:00" max="23:59" step="1" {...register('StartTime', { required: true })} />
+              </div>
+              <div className='col-lg-2'>
+                <input type='time' className='form-control' min="00:00" max="23:59" step="1" {...register('EndTime', { required: true })} />
+              </div>
+              <div className='col-lg-2'>
+                <button className='submit-button' onClick={getDetails}>Click</button>
+              </div>
+            </div><br />
+            <div className='row'>
+              <div className='col-lg-4'>
+                <p>Supporting Documents</p>
+              </div>
+              <div className='col-lg-4'>
+                <p>Purpose</p>
+              </div>
+              <div className='col-lg-4'></div>
             </div>
-            <div className='col-lg-4'>
-              <p>Actual End Time<span style={{ color: "red" }}>*</span></p>
+            <div className='row'>
+              <div className='col-lg-4'>
+                <input type='file' className='form-control' {...register('Upload', { required: true })}></input>
+              </div>
+              <div className='col-lg-4'>
+                <textarea className='form-control' placeholder='Write here...' {...register('Comments', { required: true })}></textarea>
+              </div>
+              <div className='col-lg-4'></div>
             </div>
-          </div>
-          <div className='row'>
-            <div className='col-lg-4'>
-              <input type='date' className='form-control'></input>
-            </div>
-            <div className='col-lg-1'>
-              <input type="text" maxlength="2" class="form-control input-md text-center bs-timepicker-field" placeholder="HH"></input>
-            </div>:
-            <div className='col-lg-1'>
-              <input type="text" maxlength="2" class="form-control input-md text-center bs-timepicker-field" placeholder="MM"></input>
-            </div>
-            <div className='col-lg-2'></div>
-            <div className='col-lg-1'>
-              <input type="text" maxlength="2" class="form-control input-md text-center bs-timepicker-field" placeholder="HH"></input>
-            </div>:
-            <div className='col-lg-1'>
-              <input type="text" maxlength="2" class="form-control input-md text-center bs-timepicker-field" placeholder="MM"></input>
-            </div>
-          </div><br />
-          <div className='row'>
-            <div className='col-lg-4'>
-              <p>Supporting Documents</p>
-            </div>
-            <div className='col-lg-4'>
-              <p>Purpose</p>
-            </div>
-            <div className='col-lg-4'></div>
-          </div>
-          <div className='row'>
-            <div className='col-lg-4'>
-              <input type='file' className='form-control'></input>
-            </div>
-            <div className='col-lg-4'>
-              <textarea className='form-control' placeholder='Write here...'></textarea>
-            </div>
-            <div className='col-lg-4'></div>
-          </div>
-          <div className='row mt-4'>
-            <div className='col-lg-6'></div>
-            <div className='col-lg-2'>
-              <button className='submit-button' style={{ float: "right" }}>Cancel</button>
-            </div>
-            <div className='col-lg-2'>
-              <button className='submit-button'>Submit</button>
-            </div>
-            <div className='col-lg-2'>
-              <button className='submit-button' style={{ float: "left" }}>OT Details</button>
+            <div className='row mt-4'>
+              <div className='col-lg-6'></div>
+              <div className='col-lg-2'>
+                <button className='submit-button' style={{ float: "right" }}>Cancel</button>
+              </div>
+              <div className='col-lg-2'>
+                <button className='submit-button' onClick={insertDetails}>Submit</button>
+              </div>
+              <div className='col-lg-2'>
+                <button className='submit-button' onClick={openEditModal} style={{ float: "left" }}>OT Details</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
+      <Modal isOpen={modalOpen} style={customStyles} contentLabel="Example Modal">
+        <div className='row'>
+          <div className='col-lg-6'>
+            <div className=" modal-header">
+              <h5 className=" modal-title" id="exampleModalLabel">
+                Overtime Details
+              </h5>
+            </div>
+          </div>
+          <div className='col-lg-5'></div>
+          <div className='col-lg-1'>
+            <button
+              aria-label="Close"
+              type="button"
+              className={Styles.close}
+              onClick={closeModal}
+            >X
+            </button>
+          </div>
+
+        </div>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th>OT Type</th>
+                  <th>No of Hours</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  {dashboardData.map((data, index) => {
+                    return (
+                      <>
+                        <td key={index}>
+                          <tr>
+                            <td>Normal OT</td>24
+                          </tr>
+                          <tr>
+                            <td>Night OT </td>23
+                          </tr>
+                          <tr>
+                            <td> Excess Night OT </td>22
+                          </tr>
+                          <tr>
+                            <td> Excess Normal OT </td>21
+                          </tr>
+                          <tr>
+                            <td>Rest Normal OT </td>20
+                          </tr>
+                          <tr>
+                            <td> Rest Night OT </td>19
+                          </tr>
+                          <tr>
+                            <td> Excess Rest Normal OT </td>18
+                          </tr>
+                          <tr>
+                            <td> Rest Excess Night OT </td>17
+                          </tr>
+                          <tr>
+                            <td> Legal Night OT </td>16
+                          </tr>
+                          <tr>
+                            <td> Legal Normal OT </td>15
+                          </tr>
+                          <tr>
+                            <td> Legal Excess Normal OT </td>13
+                          </tr>
+                          <tr>
+                            <td>Legal Excess Night OT </td>12
+                          </tr>
+                          <tr>
+                            <td> Special Night OT </td>11
+                          </tr>
+                          <tr>
+                            <td> Special Normal OT </td>10
+                          </tr>
+                          <tr>
+                            <td> Special Excess Normal OT </td>9
+                          </tr>
+                          <tr>
+                            <td> Special Excess Night OT </td>8
+                          </tr>
+                          <tr>
+                            <td> Special Rest Night OT </td>8
+                          </tr>
+                          <tr>
+                            <td>Special Rest Normal OT </td>7
+                          </tr>
+                          <tr>
+                            <td> Special Rest Excess Normal OT </td>6
+                          </tr>
+                          <tr>
+                            <td> Special Rest Excess Night OT </td>5
+                          </tr>
+                          <tr>
+                            <td> Legal Rest Night OT </td>4
+                          </tr>
+                          <tr>
+                            <td> Legal Rest Normal OT </td>3
+                          </tr>
+                          <tr>
+                            <td> Legal Excess Rest Normal OT </td>2
+                          </tr>
+                          <tr>
+                            <td> Legal Excess Rest Night OT </td>1
+                          </tr>
+                        </td>
+                        <td key={index}>
+                          <tr>
+                            <td>{data.normalOT}</td>1
+                          </tr>
+                          <tr>
+                            <td>{data.nightOt}</td>2
+                          </tr>
+                          <tr>
+                            <td>{data.exccessNightOt}</td>3
+                          </tr>
+                          <tr>
+                            <td>{data.exccessNormalOt}</td>4
+                          </tr>
+                          <tr>
+                            <td>{data.restNormalOT}</td>5
+                          </tr>
+                          <tr>
+                            <td>{data.restNightOt}</td>6
+                          </tr>
+                          <tr>
+                            <td>{data.exccessRestNormalOt}</td>7
+                          </tr>
+                          <tr>
+                            <td>{data.restExccessNightOt}</td>9
+                          </tr>
+                          <tr>
+                            <td>{data.legalNightOt}</td>10
+                          </tr>
+                          <tr>
+                            <td>{data.legalNormalOT}</td>11
+                          </tr>
+                          <tr>
+                            <td>{data.legalExccessNormalOt}</td>13
+                          </tr>
+                          <tr>
+                            <td>{data.legalExccessNightOt}</td>13
+                          </tr>
+                          <tr>
+                            <td>{data.specialNightOt}</td>14
+                          </tr>
+                          <tr>
+                            <td>{data.specialNormalOT}</td>14
+                          </tr>
+                          <tr>
+                            <td>{data.specialExccessNormalOt}</td>15
+                          </tr>
+                          <tr>
+                            <td>{data.specialExccessNightOt}</td>16
+                          </tr>
+                          <tr>
+                            <td>{data.specialRestNightOt}</td>17
+                          </tr>
+                          <tr>
+                            <td>{data.specialRestNormalOT}</td>18
+                          </tr>
+                          <tr>
+                            <td>{data.specialRestExccessNormalOt}</td>19
+                          </tr>
+                          <tr>
+                            <td>{data.specialRestExccessNightOt}</td>20
+                          </tr>
+                          <tr>
+                            <td>{data.legalRestNightOt}</td>21
+                          </tr>
+                          <tr>
+                            <td>{data.legalRestNormalOT}</td>22
+                          </tr>
+                          <tr>
+                            <td>{data.legalExccessRestNormalOt}</td>23
+                          </tr>
+                          <tr>
+                            <td>{data.legalExccessRestNightOt}</td>24
+                          </tr>
+                        </td>
+                      </>
+                    )
+                  })
+                  }
+
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </Modal>
     </Layout>
 
   )
