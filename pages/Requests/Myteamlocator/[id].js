1bf23a4a2a7d146ed95e2028390d1de0fd3d4f4e
krishna <krishna@amazeinc.in>
2023-05-05 18:14:40 +0530

    fixed the Request components

--- pages/Requests/Myteamlocator/[id].js
+++ pages/Requests/Myteamlocator/[id].js
@@ -1,22 +1,14 @@
+import React from 'react';
 import { useRouter } from 'next/router'
-import MyteamLocatordetails from '../Myteamlocator/new';
-import axios from 'axios';
-const Myteamlocatoredit = ({ data }) => {
-    console.log(data)
-    return (        
-        < MyteamLocatordetails editData = { data } ></MyteamLocatordetails >        
-    )
-} 
-export default Myteamlocatoredit;
+const Id = () => {
+    const router = useRouter()
+    const { id } = router.query
 
-export async function getServerSideProps(context) {
-    console.log(context);
-    const hostURL = process.env.NEXT_PUBLIC_API_HOST_URL;
-    debugger;
-    // Fetch data from external API 
-    await axios.post(hostURL + "Payroll/UpdateLocatorStatus?ID=" + context.params.id);
-    Swal.fire(" Locator Details Approved")
-    getStafflocator()
-    // Pass data to the page via props
-    return { props: { data } }
+    return (
+        <div>
+            <p> your ID: {id}</p>
+        </div>
+    );
 }
+
+export default Id;
