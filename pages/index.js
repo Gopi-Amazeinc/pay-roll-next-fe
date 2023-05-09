7b166acfbd3b5abbd26aa300081ad025bc56d19a
Gopi-amazeinc <gopi@amazeinc.in>
2023-05-09 15:37:05 +0530

    chnages

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,14 @@ import styles from '@/styles/Home.module.css'
 // import Layout from '../components/layout/index.js'
 import Layout from '@/components/layout/layout'
 
+
 export default function Home() {
 
   return (
     <>
+    <Head>
+  <link rel="icon" type="image/x-icon" href="../public/images/DigiLogoBlue.png"/>
+</Head>
       <Layout>
 
       </Layout>
