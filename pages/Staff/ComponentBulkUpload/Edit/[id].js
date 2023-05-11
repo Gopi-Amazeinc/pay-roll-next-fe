2cca9df0abb723542821ab2804be4eb1bdb9109b
Naveen <naveen.th@amazeinc.in>
2023-05-11 12:20:58 +0530

    minor

--- pages/Staff/ComponentBulkUpload/Edit/[id].js
+++ pages/Staff/ComponentBulkUpload/Edit/[id].js
@@ -4,58 +4,6 @@ import ComponentBulkUploadForm from "../../../../pages/Staff/ComponentBulkUpload
 const ComponentBulkUploadFormEdit = () => {
   const router = useRouter()
   const { id } = router.query
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   watch,
-  //   reset,
-  //   formState: { errors },
-  // } = useForm();
-  // const [staff, setStaffData] = useState([]);
-  // const [component, setComponentData] = useState([]);
-  // const [payment, setPayment] = useState([]);
-
-  // useEffect(() => {
-  //   getData();
-  //   getByID();
-  // }, [1]);
-
-  // async function getData() {
-  //   let res = await axios.get(hostURL + "HR/GetAllStaffNew");
-  //   setStaffData(res.data);
-
-
-  // }
-
-  // const getByID = async () => {
-  //   if (id) {
-  //     let response = await axios.get(
-  //       hostURL + "Payroll/GetPayrollComponentBulkUploadByID?ID=" + id
-  //     );
-  //     clearForm(response.data[0])
-  //   } else {
-  //     clearForm();
-  //   }
-  // }
-
-  // function clearForm(userData = null) {
-  //   let details = {
-  //     ID: userData ? userData.id : "",
-  //     EmployeeID: userData ? userData.employeeID : "",
-  //     PayCode: userData ? userData.payCode : "",
-  //     Amount: userData ? userData.amount : "",
-  //     Paymentfrequeicy: userData ? userData.paymentfrequeicy : "",
-  //   };
-  //   reset(details);
-  // }
-
-  // async function onSubmit(data) {
-
-  //   await axios.post(hostURL + "HR/UpdatePayrollComponentBulkUpload", data);
-  //   Swal.fire("Updated successfully");
-  //   location.href = "/Staff/ComponentBulkUpload";
-
-  // }
 
   return (
     <ComponentBulkUploadForm editData={{ id }}/>
