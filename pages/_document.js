d5c7689adefa2535a0a41a1ae54e521849a0eee5
Shashank-AmazeINC <125264768+Shashank-AmazeINC@users.noreply.github.com>
2023-05-15 18:58:23 +0530

    changes revoked

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default function Document() {
           width= "auto" height= "auto"
         />
         </Head>
-      <body style={{ backgroundColor: '#ffffff' }}>
+      <body >
         <Main />
         <NextScript />
       </body>
