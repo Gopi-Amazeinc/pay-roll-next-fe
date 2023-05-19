088286061eaadfa823ca235e6bb94316cd9c8685
Naveen <naveen.th@amazeinc.in>
2023-05-19 15:42:56 +0530

    payslip api binding in progress

--- pages/Reports/Payslip/index.js
+++ pages/Reports/Payslip/index.js
@@ -2,83 +2,105 @@ import Layout from '@/components/layout/layout.js';
 import payslipstyle from "../../../pages/Reports/Payslip/payslip.module.css"
 import { useEffect, useRef, useState } from 'react';
 import React from 'react';
+import axios from "axios";
+
 export default function PayslipReport() {
+
+    const [payroll, setPayroll] = useState([]);
+    const [payroll1, setPayroll1] = useState([]);
+    const hostURL = process.env.NEXT_PUBLIC_API_HOST_URL;
+
+    useEffect(() => {
+        getPayslips();
+      }, [1]);
+
+      const getPayslips = async () => {
+        let res = await axios.get(hostURL + "Payroll/GetEmployeeSalary");
+        
+        setPayroll(res.data);
+      }
     const [isChecked, setIsChecked] = useState(false);
-    const [isChecked1, setIsChecked1] = useState(false);
-    const handleCheckboxChange = () => {
+    // const [isChecked1, setIsChecked1] = useState(false);
+    // const handleCheckboxChange = () => {
+    //     setIsChecked(!isChecked);
+    // };
+    const payrollChecked = async (data) => {
+        debugger
+        console.log(data)
+        let res = await axios.get(hostURL + "Payroll/GetEmployeeSalary");
+
         setIsChecked(!isChecked);
     };
 
+    // const handleCheckboxChange1 = () => {
+    //     setIsChecked1(!isChecked1)
+    // };
 
-    const handleCheckboxChange1 = () => {
-        setIsChecked1(!isChecked1)
-    };
+    // const tabsData = [
+    //     {
+    //         label: 'PENDING',
+    //         content:
+    //             <div className="container-fluid mt-4">
+    //                 <div className="row">
+    //                     <table className='table  table-striped mt-3 text-center' id={payslipstyle.table} >
+    //                         <thead>
+    //                             <tr className={payslipstyle.tr}  >
+    //                                 <th>Select</th>
+    //                                 <th>Year</th>
+    //                                 <th>Month</th>
+    //                                 <th>Period</th>
+    //                                 <th>Payroll Run Type</th>
+    //                             </tr>
+    //                         </thead>
+    //                         <tbody>
+    //                             <tr >
+    //                                 <td><input type="checkbox" name="" id="" checked={isChecked} onClick={handleCheckboxChange} /></td>
+    //                                 <td>2023</td>
+    //                                 <td>3</td>
+    //                                 <td>1</td>
+    //                                 <td>Payroll For -</td>
+    //                             </tr>
+    //                         </tbody>
+    //                     </table>
+    //                 </div>
+    //             </div>,
+    //     }, {
+    //         label: 'APPROVED',
+    //         content:
+    //             <div className="container-fluid mt-4">
+    //                 <div className="row">
+    //                     <table className='table  table-striped mt-3 text-center' id={payslipstyle.table}  >
+    //                         <thead>
+    //                             <tr className={payslipstyle.tr} >
+    //                                 <th>Select</th>
+    //                                 <th>Name</th>
+    //                                 <th>Role</th>
+    //                                 <th>Payroll Run Type</th>
+    //                             </tr>
+    //                         </thead>
+    //                         <tbody>
+    //                             <td>1</td>
+    //                             <td>1</td>
+    //                             <td>1</td>
+    //                             <td>1</td>
+    //                         </tbody>
+    //                     </table>
+    //                 </div>
+    //             </div>
+    //     }
+    // ]
+    // const [activeTabIndex, setActiveTabIndex] = useState(0);
+    // const tabsRef = useRef([]);
 
-    const tabsData = [
-        {
-            label: 'PENDING',
-            content:
-                <div className="container-fluid mt-4">
-                    <div className="row">
-                        <table className='table  table-striped mt-3 text-center' id={payslipstyle.table} >
-                            <thead>
-                                <tr className={payslipstyle.tr}  >
-                                    <th>Select</th>
-                                    <th>Year</th>
-                                    <th>Month</th>
-                                    <th>Period</th>
-                                    <th>Payroll Run Type</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr >
-                                    <td><input type="checkbox" name="" id="" checked={isChecked} onClick={handleCheckboxChange} /></td>
-                                    <td>2023</td>
-                                    <td>3</td>
-                                    <td>1</td>
-                                    <td>Payroll For -</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>,
-        }, {
-            label: 'APPROVED',
-            content:
-                <div className="container-fluid mt-4">
-                    <div className="row">
-                        <table className='table  table-striped mt-3 text-center' id={payslipstyle.table}  >
-                            <thead>
-                                <tr className={payslipstyle.tr} >
-                                    <th>Select</th>
-                                    <th>Name</th>
-                                    <th>Role</th>
-                                    <th>Payroll Run Type</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <td>1</td>
-                                <td>1</td>
-                                <td>1</td>
-                                <td>1</td>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-        }
-    ]
-    const [activeTabIndex, setActiveTabIndex] = useState(0);
-    const tabsRef = useRef([]);
+    // useEffect(() => {
+    //     function setTabPosition() {
+    //         const currentTab = tabsRef.current[activeTabIndex];
+    //     }
+    //     setTabPosition();
+    //     window.addEventListener('resize', setTabPosition);
 
-    useEffect(() => {
-        function setTabPosition() {
-            const currentTab = tabsRef.current[activeTabIndex];
-        }
-        setTabPosition();
-        window.addEventListener('resize', setTabPosition);
-
-        return () => window.removeEventListener('resize', setTabPosition);
-    }, [activeTabIndex]);
+    //     return () => window.removeEventListener('resize', setTabPosition);
+    // }, [activeTabIndex]);
 
     return (
         <Layout>
@@ -89,21 +111,97 @@ export default function PayslipReport() {
                     <br></br>
                     <div className='row'>
                         <div className='col-lg-2'>
-                            <button className='button'>
+                            <button className={payslipstyle.payrollbutton}>
                                 Normal Payroll
                             </button>
                         </div>
                     </div>
-
+                    <br></br>
                     <div className='row'>
-                        <table>
-                            
+                        <div className="col-lg-12">
+                        <table className="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Select</th>
+                                    <th>Year</th>
+                                    <th>Month</th>
+                                    <th>Period</th>
+                                    <th>Payroll Run Type</th>
+                                    <th>Description</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {payroll.map((data,index)=>{
+                                    return (
+                                        <tr className='text-dark' key={index}>
+                                            <td><input type="checkbox" name="" value={data.id} id='flexCheckDefault' onClick={payrollChecked(data)} /></td>
+                                            <td>{data.endyear}</td>
+                                            <td>{data.monthname}</td>
+                                            <td>{data.payrolltype}</td>
+                                            <td>Normal</td>
+                                            <td>Payroll For -  {data.modifiedDate} - {data.endDate}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
                         </table>
+                        </div>
                     </div>
                 </div>
             </div>
 
+            {isChecked && (
+                <div className="row">
+                    <h4 className={payslipstyle.reports}>Employees in selected Period </h4>
 
+                    <div className="col-lg-2">
+                        <p>Filter By</p>
+                    </div>
+                    <div className="col-lg-4">
+                        <label htmlFor="">Search</label>
+                        {/* <input type="text" className="form-control" checked={isChecked1} /> */}
+                    </div>
+                    <div className="col-lg-3">
+                        <label htmlFor="">Role</label>
+                        <select name="" id="" className="form-control">
+                            <option value="" disabled>Select Role</option>
+                            <option value="">ADMIN</option>
+                            <option value="">Finance</option>
+                            <option value="">EP1</option>
+                        </select>
+                    </div>
+                    <div className="col-lg-3">
+                        <label htmlFor="">Department</label>
+                        <select name="" id="" className="form-control">
+                            <option value="" disabled>Select Role</option>
+                            <option value="">ADMIN</option>
+                            <option value="">Finance</option>
+                            <option value="">EP1</option>
+                        </select>
+                    </div>
+                    <table className='table  table-striped mt-3 text-center' id={payslipstyle.table} >
+                        <thead>
+                            <tr className={payslipstyle.tr}  >
+                                <th>Select</th>
+                                <th>EmployeeID</th>
+                                <th>Employee Name	</th>
+                                <th>Department</th>
+                                <th>Status</th>
+
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr >
+                                {/* <td><input type="checkbox" onClick={handleCheckboxChange1} name="" id="" /></td> */}
+                                <td>101010101</td>
+                                <td>admin</td>
+                                <td>IT</td>
+                                <td>Active</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>)}
 
 
 
