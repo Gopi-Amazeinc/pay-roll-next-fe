9672e361ed55fae0be42cc470eb75bae47ba3d4e
Gopi-amazeinc <gopi@amazeinc.in>
2023-05-20 12:09:26 +0530

    punchout

--- pages/Home/dashboard.js
+++ pages/Home/dashboard.js
@@ -53,8 +53,6 @@ const Dashboard = () => {
   const [localIPAddress, setLocalIPAddress] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
-  const [staffData,setstaffData] = useState();
-
   useEffect(() => {
     const Loginname = sessionStorage.getItem("userName");
     setUserName(Loginname);
@@ -80,9 +78,8 @@ const Dashboard = () => {
     var todayDate = new Date().toISOString().slice(0, 10);
     debugger;
     if (userID) {
-      const staffDetails = apiService.commonGetCall("HR/GetAttendance");
-      setstaffData(staffDetails)
-      staffData.data.filter(
+      apiService.commonGetCall("HR/GetAttendance").then((staffData) => {
+        const staffDetails = staffData.data.filter(
           (x) => x.filterdate == todayDate && x.userID == userID
         );
         if (staffDetails.length && staffDetails[0].signinDate != null) {
@@ -100,7 +97,7 @@ const Dashboard = () => {
         // const currentTime = new Date();
         // setPunchintime(currentTime.toLocaleTimeString());
         // setSubmitted(true);
-      // });
+      });
     }
   }, [userID]);
 
