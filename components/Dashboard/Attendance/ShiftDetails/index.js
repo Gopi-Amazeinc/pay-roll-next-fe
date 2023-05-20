d92f303a46d7ba5170cb86f1a4d56e786a693e51
Gurunath <gurunath@amazeinc.in>
2023-05-20 12:00:33 +0530

    changes in attendace correction and shift details

--- components/Dashboard/Attendance/ShiftDetails/index.js
+++ components/Dashboard/Attendance/ShiftDetails/index.js
@@ -195,17 +195,18 @@ const Shiftdetails = () => {
 
               <div className="col-lg-2">
                 <br />
-                <Link href="/Attendance/StaffShiftForm/new" className={Styles.adddetail}>
-                  <button className="button" style={{ fontSize: "15px", marginTop: "7px" }}><IoIosAddCircleOutline size={18} color={"white"} />  ADD SHIFT DETAILS</button>
-                </Link>
-              </div>
-              <div className="col-lg-2">
-                <br />
                 <DownloadTableExcel
                   filename="users table"
                   sheet="users"
                   currentTableRef={tableRef.current}
                 > <button className="button" style={{ marginTop: "7px" }} > DOWNLOAD</button></DownloadTableExcel>
+              </div>
+              <div className="col-lg-2">
+                <br />
+                <Link href="/Attendance/StaffShiftForm/new" className={Styles.adddetail}>
+                  <button className="button" style={{ fontSize: "15px", marginTop: "7px" }}><IoIosAddCircleOutline size={18} color={"white"} />  ADD SHIFT DETAILS</button>
+                </Link>
+
 
               </div>
             </div>
