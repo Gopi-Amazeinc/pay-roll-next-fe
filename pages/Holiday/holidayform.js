a6073cc354a0bb42be9b57d0f8cfcc6a38814f44
Shashank-AmazeINC <125264768+Shashank-AmazeINC@users.noreply.github.com>
2023-05-23 14:38:30 +0530

    Admin login changes completed

--- pages/Holiday/holidayform.js
+++ pages/Holiday/holidayform.js
@@ -9,7 +9,7 @@ import { apiService } from "@/services/api.service";
 import { useRouter } from "next/router";
 
 
-function Holidayform({editData}) {
+function Holidayform({ editData }) {
 
   const { register, handleSubmit, watch, reset, formState: { errors }, } = useForm();
   let hostURL = process.env.NEXT_PUBLIC_API_HOST_URL;
@@ -86,87 +86,91 @@ function Holidayform({editData}) {
 
 
   return (
-    <div>
-      <Layout>
-        <div>
-          <h3 className="text-primary fs-5 mt-3">Holidays</h3>
-          <div class="shadow-lg p-3 mb-5 bg-white rounded">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="row">
-                <div className="col-lg-4">
-                  <p>
-                    Holiday<i className="text-danger">*</i>
-                  </p>
-                  <input type="text" className="form-control" placeholder="Holiday"{...register('Holiday', { required: "Please add a Short Name", pattern: { value: /^[A-Za-z0-9]+$/, message: "Please enter a valid Short Name" } })} />
-                  {errors.Name && <p className="error-message" style={{ color: "red" }}>{errors.Name.message}</p>}
-                </div>
+    <Layout>
+      <div className='container-fluid'>
+        <p className='Heading'>Holidays</p>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <div className='card border-0 p-3 rounded-3'>
+              <div className='row'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="row">
+                    <div className="col-lg-4">
+                      <p>
+                        Holiday<i className="text-danger">*</i>
+                      </p>
+                      <input type="text" className="form-control" placeholder="Holiday"{...register('Holiday', { required: "Please add a Short Name", pattern: { value: /^[A-Za-z0-9]+$/, message: "Please enter a valid Short Name" } })} />
+                      {errors.Name && <p className="error-message" style={{ color: "red" }}>{errors.Name.message}</p>}
+                    </div>
 
-                <div className="col-lg-4">
-                  <p>
-                    Holiday Description<i className="text-danger">*</i>
-                  </p>
-                  <textarea
-                    className="form-control"
-                    placeholder="Holiday Description"
-                    {...register('HolidayDescription', { required: "Please add a Descrption Name", pattern: { value: /^[A-Za-z0-9]+$/, message: "Please enter a valid Descrption Name" } })}
-                  ></textarea>
-                  {errors.Name && <p className="error-message" style={{ color: "red" }}>{errors.Name.message}</p>}
-                </div>
+                    <div className="col-lg-4">
+                      <p>
+                        Holiday Description<i className="text-danger">*</i>
+                      </p>
+                      <textarea
+                        className="form-control"
+                        placeholder="Holiday Description"
+                        {...register('HolidayDescription', { required: "Please add a Descrption Name", pattern: { value: /^[A-Za-z0-9]+$/, message: "Please enter a valid Descrption Name" } })}
+                      ></textarea>
+                      {errors.Name && <p className="error-message" style={{ color: "red" }}>{errors.Name.message}</p>}
+                    </div>
 
-                <div className="col-lg-4">
-                  <p>
-                    Holiday Date<i className="text-danger">*</i>
-                  </p>
-                  <input type="date" className="form-control" placeholder="Holiday Date"{...register('HolidayDate', { required: "true" })} />
-                  {errors.Name && <p className="error-message" style={{ color: "red" }}>{errors.Name.message}</p>}
-                </div>
+                    <div className="col-lg-4">
+                      <p>
+                        Holiday Date<i className="text-danger">*</i>
+                      </p>
+                      <input type="date" className="form-control" placeholder="Holiday Date"{...register('HolidayDate', { required: "true" })} />
+                      {errors.Name && <p className="error-message" style={{ color: "red" }}>{errors.Name.message}</p>}
+                    </div>
+                  </div>
+
+
+                  <div className="row">
+                    <div className="col-lg-4">
+                      <p>
+                        Attachment<i className="text-danger">*</i>
+                      </p>
+                      <input type="text" className="form-control" placeholder="Attachment"{...register('Attachment', { required: "Please add a Short Name", pattern: { message: "Please enter a valid Short Name" } })} />
+                      {errors.Name && <p className="error-message" style={{ color: "red" }}>{errors.Name.message}</p>}
+                    </div>
+                    <div className="col-lg-4">
+                      <p>
+                        Holiday Category<i className="text-danger">*</i>
+                      </p>
+                      <input type="text" className="form-control" placeholder="Holiday Category"{...register('HolidayCategory', { required: "Please add a Short Name", pattern: { value: /^[A-Za-z0-9]+$/, message: "Please enter a valid Short Name" } })} />
+                      {errors.Name && <p className="error-message" style={{ color: "red" }}>{errors.Name.message}</p>}
+                    </div>
+                    <div className="col-lg-4">
+                      <p>
+                        Region<i className="text-danger">*</i>
+                      </p>
+                      <input type="text" className="form-control" placeholder="Region"{...register('Region', { required: "Please add a Short Name", pattern: { value: /^[A-Za-z0-9]+$/, message: "Please enter a valid Short Name" } })} />
+                      {errors.Name && <p className="error-message" style={{ color: "red" }}>{errors.Name.message}</p>}
+                    </div>
+                  </div>
+                  <br />
+                  <div className="row">
+                    <div className="col-lg-8"></div>
+                    <div className="col-lg-2">
+                      <Link href="/Holiday">
+                        <button type="submit" className="AddButton">Cancel</button>
+                      </Link>
+                    </div>
+                    <div className="col-lg-2 ">
+                      {actionType == "insert" && (
+                        <button type="submit" className="AddButton"> Save</button>)}
+
+                      {actionType == "update" && (
+                        <button type="submit" className="AddButton">  Update </button>)}
+                    </div>
+                  </div>
+                </form>
               </div>
-
-
-              <div className="row">
-                <div className="col-lg-4">
-                  <p>
-                    Attachment<i className="text-danger">*</i>
-                  </p>
-                  <input type="text" className="form-control" placeholder="Attachment"{...register('Attachment', { required: "Please add a Short Name", pattern: { message: "Please enter a valid Short Name" } })} />
-                  {errors.Name && <p className="error-message" style={{ color: "red" }}>{errors.Name.message}</p>}
-                </div>
-                <div className="col-lg-4">
-                  <p>
-                    Holiday Category<i className="text-danger">*</i>
-                  </p>
-                  <input type="text" className="form-control" placeholder="Holiday Category"{...register('HolidayCategory', { required: "Please add a Short Name", pattern: { value: /^[A-Za-z0-9]+$/, message: "Please enter a valid Short Name" } })} />
-                  {errors.Name && <p className="error-message" style={{ color: "red" }}>{errors.Name.message}</p>}
-                </div>
-                <div className="col-lg-4">
-                  <p>
-                    Region<i className="text-danger">*</i>
-                  </p>
-                  <input type="text" className="form-control" placeholder="Region"{...register('Region', { required: "Please add a Short Name", pattern: { value: /^[A-Za-z0-9]+$/, message: "Please enter a valid Short Name" } })} />
-                  {errors.Name && <p className="error-message" style={{ color: "red" }}>{errors.Name.message}</p>}
-                </div>
-              </div>
-
-              <div className="row mt-5">
-                <div className="col-lg-8"></div>
-                <div className="col-lg-2  text-end">
-                  <Link href="/Holiday">
-                    <button type="submit" className="AddButton">Cancel</button>
-                  </Link>
-                </div>
-                <div className="col-lg-2 ">
-                  {actionType == "insert" && (
-                    <button type="submit" className="AddButton"> Save</button>)}
-
-                  {actionType == "update" && (
-                    <button type="submit" className="AddButton">  Update </button>)}
-                </div>
-              </div>
-            </form>
+            </div>
           </div>
         </div>
-      </Layout>
-    </div>
+      </div>
+    </Layout>
   )
 }
 
