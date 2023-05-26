1396b8c7e438f7ac27aa976678fa05b3c6a0f5f5
Naveen <naveen.th@amazeinc.in>
2023-05-26 11:43:18 +0530

    Upload functionality added into staff, waiting for API

--- components/Dashboard/Staff/StaffDashboard/index.js
+++ components/Dashboard/Staff/StaffDashboard/index.js
@@ -4,9 +4,16 @@ import Link from "next/link";
 import { BiEdit } from "react-icons/bi";
 import Styles from "../../../../styles/addStaff.module.css";
 import ReactPaginate from "react-paginate";
+import { useRef } from "react";
+import { DownloadTableExcel } from "react-export-table-to-excel";
+import Modal from "react-modal";
+import * as XLSX from "xlsx";
 
 function StaffDashbaord() {
   const [staff, setStaffData] = useState([]);
+  const tableRef = useRef(null);
+  const [count, setcount] = useState("");
+  const [keyword, setKeyword] = useState("");
   const hostURL = process.env.NEXT_PUBLIC_API_HOST_URL;
 
   useEffect(() => {
@@ -18,6 +25,7 @@ function StaffDashbaord() {
       hostURL + "Payroll/GetAllStaffNewforstaffdashboard"
     );
     setStaffData(res.data);
+    setcount(res.data.length);
   };
   const getData = (data) => {
     // sessionStorage.setItem("id", data.id);
@@ -26,12 +34,13 @@ function StaffDashbaord() {
     // sessionStorage.setItem("id", "");
   };
   const enableDisableStaff = async (data) => {
+    debugger;
     let entity = {
       StaffID: data.employeID,
-      AttendanceEnable: !data.attendanceEnable,
+      AttendanceEnable: 1,
     };
     await axios.post(hostURL + "Payroll/UpdateAttendanceEnableDisable", entity);
-    if (etty.AttendanceEnable == true) {
+    if (entity.AttendanceEnable == true) {
       Swal.fire("Attendance enabled");
     } else {
       Swal.fire("Attendance disabled");
@@ -49,8 +58,29 @@ function StaffDashbaord() {
       Swal.fire("Failed to delete data");
     }
   };
+  const customStyles = {
+    content: {
+      top: "20%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      width: "30%",
+      overflow: "hidden",
+    },
+    errorMsg: {
+      fontSize: "12px",
+      fontWeight: "500",
+      color: "red",
+    },
+    inputLabel: {
+      fontSize: "16px",
+    },
+  };
 
-  const PER_PAGE = 7;
+
+  const PER_PAGE = 7; //pagination
   const [currentPage, setCurrentPage] = useState(0);
   function handlePageClick({ selected: selectedPage }) {
     setCurrentPage(selectedPage);
@@ -59,6 +89,80 @@ function StaffDashbaord() {
   const pageCount = Math.ceil(staff.length / PER_PAGE);
 
 
+  const [modalOpen, setModalOpen] = useState(false); //modal
+  const openEditModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+
+
+  const incomingfile = async (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        const data = XLSX.utils.sheet_to_json(ws);
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+    promise.then((d) => {
+      setItems(d);
+    });
+  };
+
+  const uploadStaff = async () => {
+    const transformedData = await transformedStaff(items);
+    if (transformedData.length > 0) {
+      await apiService.commonPostCall(
+        "Payroll/InsertStaffOvetimeOTupload",
+        transformedData
+      );
+      Swal.fire("Component Bulk Uploaded Successfully!");
+    } else {
+      Swal.fire("Upload failed!");
+    }
+    setModalOpen(false);
+    getStaffSalary();
+  };
+
+  const transformedStaff = async (items) => {
+    console.log(items);
+    debugger;
+    const loans = await Promise.all(
+      items && items.length > 0
+        ? items.map(async (salary) => {
+            const res = await apiService.commonGetCall(
+              "Payroll/GetStaffByEmployeeID?EmployeID=" + salary.EmployeeID
+            );
+            let staffData;
+            // const staffData = res.data[0];
+            if (res.length != 0) {
+              staffData = res[0].id;
+            } else {
+              staffData = 0;
+            }
+            return {
+              StaffID: staffData,
+              OT_name: ot.name,
+              Hours: ot.hours,
+              PayDate: ot.Date,
+            };
+          })
+        : []
+    );
+    return loans;
+  };
   return (
     <div>
       <div className="container">
@@ -109,47 +213,53 @@ function StaffDashbaord() {
                 type="text"
                 className="form-control"
                 placeholder="Search for Staff"
+                onChange={(e) => setKeyword(e.target.value)}
               />
             </div>
 
             <div className="col-lg-2">
-              <button type="button" className="AddButton">
-                Export Excel
-              </button>
+              {count > 0 ? (
+                <>
+                  <DownloadTableExcel
+                    filename="users table"
+                    sheet="users"
+                    currentTableRef={tableRef.current}
+                  >
+                    <button className="button" id="AddButton">
+                      Download
+                    </button>
+                  </DownloadTableExcel>
+                </>
+              ) : null}
             </div>
           </div>
         </div>
         <br></br>
         <div className="row">
-          <div className="col-lg-6">
-          </div>
+          <div className="col-lg-6"></div>
           <div className="col-lg-2"></div>
           <div className="col-lg-2">
             <Link href="/Staff/AddStaff">
               <button
                 type="button"
                 className="AddButton"
-                style={{ marginTop: "5%" }}
               >
                 Add Staff
               </button>
             </Link>
           </div>
           <div className="col-lg-2">
-            <button
-              type="button"
-              className="AddButton"
-              style={{ marginTop: "5%" }}
-            >
-              Upload Staff
-            </button>
+          <button className="AddButton"
+           onClick={openEditModal}>
+                Upload Staff
+              </button>
           </div>
         </div>
 
         <div className="row mt-4">
           <div className="col-lg-12">
             <div className="table-responsive">
-              <table className="table table-striped table-bordered ">
+              <table className="table" ref={tableRef}>
                 <thead className={"bg-info text-white "}>
                   <tr style={{ whiteSpace: "nowrap" }}>
                     <th>Employee Id</th>
@@ -166,40 +276,47 @@ function StaffDashbaord() {
                   </tr>
                 </thead>
                 <tbody>
-                  {staff.slice(offset, offset + PER_PAGE).map((data, index) => {
-                    return (
-                      <tr className="text-dark" key={index}>
-                        <td>{data.employeID}</td>
-                        <td>{data.firstName}</td>
-                        <td>{data.department_name}</td>
-                        <td>{data.level}</td>
-                        <td>{data.gender}</td>
-                        <td>{data.position}</td>
-                        <td>{data.emailID}</td>
-                        <td>{data.hiredDate}</td>
-                        <td>{data.manager}</td>
-                        <td className="text-center">
-                          <span onClick={() => enableDisableStaff(data)}>
-                            {data.attendanceEnable ? (
-                              <button
-                                onClick={getData.bind(this, data)}
-                                className="enableDisableBtn"
-                              >
-                                DISABLE
-                              </button>
-                            ) : (
-                              <button
-                                onClick={getData.bind(this, data)}
-                                className="enableDisableBtn"
-                              >
-                                ENABLE
-                              </button>
-                            )}
-                          </span>
-                        </td >
-                        <td className="text-center">
-                          <Link href={`/Staff/AddStaff/Edit/${data.id}`}>
-                            {/* <buttton style={{
+                  {staff
+                    .slice(offset, offset + PER_PAGE)
+                    .filter(post => {
+                      return Object.values(post).some(value =>
+                        value !== null && value.toString().toLowerCase().includes(keyword.toLowerCase())
+                      );
+                    })
+                    .map((data, index) => {
+                      return (
+                        <tr className="text-dark" key={index}>
+                          <td>{data.employeID}</td>
+                          <td>{data.firstName}</td>
+                          <td>{data.department_name}</td>
+                          <td>{data.level}</td>
+                          <td>{data.gender}</td>
+                          <td>{data.position}</td>
+                          <td>{data.emailID}</td>
+                          <td>{data.hiredDate}</td>
+                          <td>{data.manager}</td>
+                          <td className="text-center">
+                            <span onClick={() => enableDisableStaff(data)}>
+                              {data.attendanceEnable ? (
+                                <button
+                                  onClick={getData.bind(this, data)}
+                                  className="enableDisableBtn"
+                                >
+                                  DISABLE
+                                </button>
+                              ) : (
+                                <button
+                                  onClick={getData.bind(this, data)}
+                                  className="enableDisableBtn"
+                                >
+                                  ENABLE
+                                </button>
+                              )}
+                            </span>
+                          </td>
+                          <td className="text-center">
+                            <Link href={`/Staff/AddStaff/Edit/${data.id}`}>
+                              {/* <buttton style={{
                               textShadow: "none",
                               letterSpacing: ".5px",
                               borderRadius: "5px",
@@ -211,14 +328,14 @@ function StaffDashbaord() {
                               height: "26px",
                               border: "2px solid #3247d5"
                                   }}>Edit</buttton> */}
-                            <div style={{ width: "50px" }}>
-                              <BiEdit />
-                            </div>
-                          </Link>
-                        </td>
-                      </tr>
-                    );
-                  })}
+                              <div style={{ width: "50px" }}>
+                                <BiEdit />
+                              </div>
+                            </Link>
+                          </td>
+                        </tr>
+                      );
+                    })}
                 </tbody>
               </table>
 
@@ -243,6 +360,64 @@ function StaffDashbaord() {
                   activeClassName={"active primary"}
                 />
               </div>
+
+              <Modal
+                  isOpen={modalOpen}
+                  style={customStyles}
+                  contentLabel="Example Modal"
+                >
+                  <div className=" modal-header">
+                    <h5 className=" modal-title" id="exampleModalLabel">
+                      Upload staff
+                    </h5>
+                    <button
+                      ariaLabel="Close"
+                      // className={Styles.close}
+                      type="button"
+                      onClick={closeModal}
+                    >
+                      X
+                    </button>
+                  </div>
+                  <hr></hr>
+                  <div className="row">
+                    <div className="col-lg-7">
+                      <input
+                        type="file"
+                        accept=".xls,.xlsx"
+                        style={{ display: "inline-block" }}
+                        onChange={(e) => {
+                          const file = e.target.files[0];
+                          incomingfile(file);
+                        }}
+                        placeholder="Upload file"
+                      />
+                    </div>
+                    <div className="col-lg-5">
+                      <Link href="https://103.12.1.76/ALIAPI/Images/.xlsx">
+                        <span
+                          style={{ color: "navy", textDecoration: "underline" }}
+                        >
+                          UploadTemplate.XLSX
+                        </span>
+                      </Link>
+                    </div>
+                    <div className="row">
+                      {/* <ModalFooter> */}
+                      <div className="col-lg-6">
+                        <button
+                          // className="mt-4"
+                          className="AddButton mt-4"
+                          onClick={() => uploadSalary()}
+                          color="primary"
+                          type="button"
+                        >
+                          UPLOAD
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </Modal>
             </div>
           </div>
         </div>
